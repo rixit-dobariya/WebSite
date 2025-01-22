18d921ad1c1bf32b4b717ab175835b92af11ce57
Rixit Dobariya <Tpxitachi05@outlook.com>
2025-01-22 11:10:19 +0530

    Add project files.

---
+++ WebSite/MathOps.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MathOps.aspx.cs" Inherits="MathOps" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .error{
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Number 1: <asp:TextBox ID="txtNo1" runat="server"></asp:TextBox>  <br />
+            <asp:RequiredFieldValidator CssClass="error" ControlToValidate="txtNo1" ID="RequiredFieldValidator1" runat="server" ErrorMessage="First number must not be empty!"></asp:RequiredFieldValidator>
+            <br />
+            Number 2:<asp:TextBox ID="txtNo2" runat="server"></asp:TextBox>  <br />
+            <asp:RequiredFieldValidator CssClass="error" ControlToValidate="txtNo2" ID="RequiredFieldValidator2" runat="server" ErrorMessage="Second number must not be empty"></asp:RequiredFieldValidator>
+            <br />
+            <asp:Button ID="btnAdd" runat="server" Text="Addition" OnClick="btnAdd_Click" /> &nbsp;
+            <asp:Button ID="btnSub" runat="server" Text="Subtraction" OnClick="btnSub_Click" style="height: 26px" /> &nbsp;
+            <asp:Button ID="btnMul" runat="server" Text="Multiplication" OnClick="btnMul_Click" /> &nbsp;
+            <asp:Button ID="btnDiv" runat="server" Text="Division" OnClick="btnDiv_Click" /> &nbsp;
+            <asp:Button ID="btnReset" runat="server" Text="Reset" OnClick="btnReset_Click" /> &nbsp;
+            <br />
+            <asp:Label ID="lblAnswer" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite/MathOps.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MathOps : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    void performOperation(char operation)
+    {
+        int num1 = Convert.ToInt32(txtNo1.Text);
+        int num2 = Convert.ToInt32(txtNo2.Text);
+        try
+        {
+            switch (operation)
+            {
+                case '+':
+                    lblAnswer.Text = "Addition: " + (num1 + num2).ToString();
+                    break;
+                case '-':
+                    lblAnswer.Text = "Subtraction: " + (num1 - num2).ToString();
+                    break;
+                case '*':
+                    lblAnswer.Text = "Multiplication: " + (num1 * num2).ToString();
+                    break;
+                case '/':
+                    lblAnswer.Text = "Division: " + (num1 / num2).ToString();
+                    break;
+            }
+        }
+        catch(DivideByZeroException e)
+        {
+            lblAnswer.Text = "You cannot divide number by 0";
+        }
+        
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        performOperation('+');
+    }
+
+    protected void btnSub_Click(object sender, EventArgs e)
+    {
+        performOperation('-');
+
+    }
+
+    protected void btnMul_Click(object sender, EventArgs e)
+    {
+        performOperation('*');
+
+    }
+
+    protected void btnDiv_Click(object sender, EventArgs e)
+    {
+        performOperation('/');
+
+    }
+
+    protected void btnReset_Click(object sender, EventArgs e)
+    {
+        txtNo1.Text = "";
+        txtNo2.Text = "";
+        lblAnswer.Text = "";
+    }
+}
